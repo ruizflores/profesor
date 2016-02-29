9809080a8b4d657df2740533fb124f7fb07206a9
james <floresrui123@gmail.com>
2016-02-29 15:48:16 -0500

    parcial

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>tarea</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<marquee>
+	<h1>hola lobas</h1>
+	<h1>hola lobas</h1>
+	<h1>hola lobas</h1>
+	<marquee/>
+</body>
+</html>
